8987b0224ea6f16c788633db17baaa20fa3e566b
woowahan-pjs <jason@woowahan.com>
2023-09-22 12:30:03 +0900

    refactor(mission-utils): remove test class orderer

--- src/test/java/camp/nextstep/edu/missionutils/acceptance/ScannerBufferTest.java
+++ src/test/java/camp/nextstep/edu/missionutils/acceptance/ScannerBufferTest.java
@@ -2,24 +2,19 @@ package camp.nextstep.edu.missionutils.acceptance;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
-@TestClassOrder(ClassOrderer.OrderAnnotation.class)
 class ScannerBufferTest extends NsTest {
     private static final String ERROR_CAUSING_MESSAGE = "ERROR";
     private static final String NORMAL_MESSAGE = "MESSAGE";
 
-    @Order(1)
     @TestMethodOrder(MethodOrderer.MethodName.class)
     @Nested
     class RunExceptionTest {
@@ -42,7 +37,6 @@ class ScannerBufferTest extends NsTest {
         }
     }
 
-    @Order(2)
     @TestMethodOrder(MethodOrderer.MethodName.class)
     @Nested
     class RunTest {
